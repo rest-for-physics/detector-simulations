04f44e1b566bb373f4f507a933fc62dc40780054
Cristian Cogollos <cristian@Cristians-MacBook-Pro.local>
2020-09-14 17:57:27 +0200

    Adding studies folder.

---
+++ studies/ElectronicsBG.sh
@@ -0,0 +1,37 @@
+########################################
+# Library of Baby-IAXO simulations- paths
+########################################
+export BabyIAXO_PATH="$HOME/GitLab/BabyIAXO" ## Change this to your GitLab source directory.
+export BabyIAXO_SIMPATH="$BabyIAXO_PATH/iaxo-simulations/simulations"
+export BabyIAXO_GEOMETRY="$BabyIAXO_PATH/iaxo-geometry/BabyIAXO/reference/Setup.gdml"
+
+########################################
+# Library of Baby-IAXO simulations- variables
+########################################
+#export GAS="Argon_ISO"
+#export PRESSURE="4.0" #in bar
+#export QUENCHER="0.02" #fraction of 1
+export REST_NEVENTS="1e6"
+
+#####################################################################################
+## FullChain simulations
+## Isotope options Co60, K40, Cs137, Th232, U238
+#####################################################################################
+export REST_ISOTOPE="U238" #More options 
+
+#####################################################################################
+## Volume options: AGET, Capacitor, Resistor, Diode.
+
+
+#####################################################################################
+##Full Chain From Point Source
+#####################################################################################
+export POS_X="0"
+export POS_Y="7"
+export POS_Z="20"
+restG4 $BabyIAXO_SIMPATH/background/IsotopePoint.rml
+
+
+##############
+
+###### Add other simulation definitions here
